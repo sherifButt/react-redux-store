5732608decf754683cfa3c966f19dc0ab3ef2cab
loyalleads <sherif.butt@gmail.com>
2021-09-12 10:23:27 +0100

    Link to home added to Header

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,8 +1,13 @@
+import { Link,useLocation } from 'react-router-dom'
+
 const Header = () => {
+   const location = useLocation();
+   const renderLogo = location.pathname === "/" ? 'FakeShop' : <Link to="/" >FakeShop</Link>
+
    return (
       <div className="ui fixed menu">
          <div className="ui container center">
-            <h2>FakeShop</h2>
+            <h2>{renderLogo} </h2>
          </div>
       </div>
    )
